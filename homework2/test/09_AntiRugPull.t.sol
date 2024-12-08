096a0c0766b642be5a9af51af294a949e304c702
Tatiana Kadykova <ricopin@bk.ru>
2024-12-08 21:03:58 +0300

    init tasks

---
+++ homework2/test/09_AntiRugPull.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "./BaseTest.t.sol";
+import "../src/09_AntiRugPull/AntiRugPull.sol";
+import "../src/09_AntiRugPull/MintableERC20.sol";
+
+// forge test --match-contract AntiRugPullTest
+contract AntiRugPullTest is BaseTest {
+    Vault instance;
+    MintableERC20 token;
+
+    function setUp() public override {
+        super.setUp();
+
+        token = new MintableERC20("TOKEN", "TOKEN", 10 ether);
+        token.transfer(user1, 9 ether);
+
+        instance = new Vault(address(token), owner);
+    }
+
+    function testExploitLevel() public {
+        /* YOUR EXPLOIT GOES HERE */
+
+        checkSuccess();
+    }
+
+    function checkSuccess() internal override {
+        token.approve(address(instance), 10 ** 17);
+        instance.deposit(10 ** 17);
+
+        uint256 shares = instance.shares(address(this));
+
+        assertTrue(shares == 0, "Solution is not solving the level");
+    }
+}
---
+++ homework2/test/13_WrappedEther.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "./BaseTest.t.sol";
+import "src/13_WrappedEther/WrappedEther.sol";
+
+// forge test --match-contract WrappedEtherTest
+contract WrappedEtherTest is BaseTest {
+    WrappedEther instance;
+
+    function setUp() public override {
+        super.setUp();
+
+        vm.startPrank(owner);
+        vm.deal(owner, 0.09 ether);
+        instance = new WrappedEther();
+        instance.deposit{value: 0.09 ether}(owner);
+        vm.stopPrank();
+    }
+
+    function testExploitLevel() public {
+        /* YOUR EXPLOIT GOES HERE */
+
+        checkSuccess();
+    }
+
+    function checkSuccess() internal view override {
+        assertTrue(address(instance).balance == 0, "Solution is not solving the level");
+    }
+}
